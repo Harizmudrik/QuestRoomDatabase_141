f9e03cec926ea8e7ee0e80a3cbd20b5eb228beb1
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-16 13:37:44 +0700

    Menambahkan fungsi yang mengambil nilai NIM dari savedStateHandle, yang diteruskan dari navigasi sebelumnya. Fungsi checkNotNull memastikan bahwa nilai NIM tidak null. Jika null, akan memunculkan exception.

--- app/src/main/java/com/example/prak7pam/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/prak7pam/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,2 +1,24 @@
 package com.example.prak7pam.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.prak7pam.data.entity.Mahasiswa
+import com.example.prak7pam.repository.RepositoryMhs
+import com.example.prak7pam.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class DetailMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+) : ViewModel() {
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
