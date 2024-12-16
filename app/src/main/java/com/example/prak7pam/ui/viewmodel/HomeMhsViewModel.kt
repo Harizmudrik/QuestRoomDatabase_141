824a7ef6a4949d2afe826762457fce8f486dd59c
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-16 13:27:36 +0700

    menambahkan class "HomeMhsViewModel": Sebuah kelas turunan dari ViewModel yang bertanggung jawab atas pengelolaan data dan logika bisnis untuk layar "Home".

--- app/src/main/java/com/example/prak7pam/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/prak7pam/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,2 +1,18 @@
 package com.example.prak7pam.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.prak7pam.data.entity.Mahasiswa
+import com.example.prak7pam.repository.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel()
